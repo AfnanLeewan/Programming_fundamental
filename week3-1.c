18eaef04a7e060a8cea3e00400e30e559a742a1d
AfnanLeewan <88645977+AfnanLeewan@users.noreply.github.com>
2021-08-18 11:31:44 +0700

    Add files via upload

---
+++ week3-1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+float a1,a2,a3,a4,a5,a6,a7,a8,a9,a10;
+scanf("%f %f %f %f %f %f %f %f %f %f",&a1,&a2,&a3,&a4,&a5,&a6,&a7,&a8,&a9,&a10);
+printf("ความสูงของนักเรียนตั้งเเต่คนที่ 1 ถึงคนที่ 10: %.2fcm %.2fcm %.2fcm %.2fcm %.2fcm %.2fcm %.2fcm %.2fcm %.2fcm %.2fcm",a1 ,a2,a3,a4,a5,a6,a7,a8,a9,a10);
+printf("\nความสูงเฉลี่ยของนักเรียน10คน : %fcm",(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10)/10);
+
+
+
+
+
+
+    return 0;
+}
